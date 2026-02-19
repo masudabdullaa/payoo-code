1be0ec37fd1696fd5adab79985a0d97195c7f9b2
masudabdullaa <masudabdullaa@gmail.com>
2026-02-19 23:04:25 +0600

    cashout and add money section toggle feature implimented

--- script/machine.js
+++ script/machine.js
@@ -17,3 +17,15 @@ function setBalance(value){
     const balanceElement = document.getElementById("balance");
     balanceElement.innerText = value;
 }
+// machine id > hide all > show id
+function showOnly(id){
+    console.log("show only function clicked");
+    
+    const addMoney = document.getElementById("add-money");
+    const cashout = document.getElementById("cashout");
+    addMoney.classList.add("hidden");
+    cashout.classList.add("hidden");
+
+    const selected = document.getElementById(id);
+    selected.classList.remove("hidden");
+}
